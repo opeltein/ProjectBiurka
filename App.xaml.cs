59ebb7b1d8b17a58a11dba4828de911014f71fea
opeltein <61658879+opeltein@users.noreply.github.com>
2021-02-26 14:47:34 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+       
+      
+        
+    }
+}
