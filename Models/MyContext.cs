63dbc4d6f2542d221dab002fa47ae9ac814106d4
opeltein <61658879+opeltein@users.noreply.github.com>
2021-02-26 15:09:05 +0100

    context

---
+++ Models/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Project2.Models
+{
+    class MyContext : DbContext
+    {
+        public DbSet<Biurko> Biurka { get; set; }
+        public DbSet<Pracownik> Pracownicy { get; set; }
+        public DbSet<Producent> Producenci { get; set; }
+        public DbSet<Pomieszczenie> Pomieszczenia { get; set; }
+
+  
+
+
+
+
+
+        }
+    }
+
+
+ 
+}
+
