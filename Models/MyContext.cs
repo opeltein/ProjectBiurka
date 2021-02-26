48de323475e99d08d55f653b0009ab07da50dfa1
opeltein <61658879+opeltein@users.noreply.github.com>
2021-02-26 15:10:32 +0100

    context2

--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -12,7 +12,33 @@ namespace Project2.Models
         public DbSet<Producent> Producenci { get; set; }
         public DbSet<Pomieszczenie> Pomieszczenia { get; set; }
 
-  
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Firma.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<Biurko>().ToTable("Biurka", "Biurka");
+            modelBuilder.Entity<Pracownik>().ToTable("Pracownicy", "Pracownicy");
+            modelBuilder.Entity<Producent>().ToTable("Producenci", "Producenci");
+            modelBuilder.Entity<Pomieszczenie>().ToTable("Pomieszczenia", "Pomieszczenia");
+
+            modelBuilder.Entity<Biurko>(entity =>
+            {
+                entity.HasKey(e => e.BiurkoId);
+                entity.HasIndex(e => e.Numer).IsUnique();
+
+
+
+            });
+            base.OnModelCreating(modelBuilder);
+
+
 
 
 
